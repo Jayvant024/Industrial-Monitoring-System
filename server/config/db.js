2dfb0aa3fe65dc66457caca27f51a470300d0e09
Yash Kangralkar <jaykangralkar024@gmail.com>
2026-08-23 01:05:47 +0530

    Fix MySQL database connection

--- server/config/db.js
+++ server/config/db.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
@@ -10,11 +11,11 @@ const db = mysql.createConnection({
 
 db.connect((err) => {
   if (err) {
-    console.error("❌ Database Connection Failed:", err.message);
+    console.error("❌ MySQL connection failed:", err.message);
     return;
   }
 
-  console.log("✅ MySQL Connected Successfully");
+  console.log("✅ MySQL database connected successfully");
 });
 
 module.exports = db;
