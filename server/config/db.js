140da5dbfe2e9394950c862ba55df43e4c010254
Yash Kangralkar <jaykangralkar024@gmail.com>
2026-08-23 09:29:11 +0530

    Fix MySQL connection pool

--- server/config/db.js
+++ server/config/db.js
@@ -1,21 +1,30 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT) || 3306,
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0
 });
 
-db.connect((err) => {
+// Test database connection
+db.getConnection((err, connection) => {
   if (err) {
     console.error("❌ MySQL connection failed:", err.message);
     return;
   }
 
   console.log("✅ MySQL database connected successfully");
+
+  connection.release();
 });
 
 module.exports = db;
